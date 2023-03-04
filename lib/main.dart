54a414b32d94384950fa54c35d5c6e6f46bc1c43
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-03-04 15:43:27 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_test/test.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Testing(),
+    );
   }
 }
