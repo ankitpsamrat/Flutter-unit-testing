da0efbc51cfe471bb387d748dc6cb0329c4f282a
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-04-22 23:09:39 +0530

    changes

--- lib/counter.dart
+++ lib/counter.dart
@@ -8,4 +8,8 @@ class Counter {
   void incrementCounter() {
     _counter++;
   }
+
+  void decrementCounter() {
+    _counter--;
+  }
 }
--- test/counter_test.dart
+++ test/counter_test.dart
@@ -1,5 +1,47 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:just_test/counter.dart';
 
 void main() {
-  test('description', () => null);
+  // this method is used for only one test case.
+  // test(
+  //     'When counter class instantiated then the value of count should be 0', // this is just a msg
+  //     () {
+  //   final Counter counter = Counter();
+
+  //   final val = counter.count;
+
+  //   expect(val, 0);
+  // });
+
+  // this method is used for multiple test cases.
+  group('Counter Test', () {
+    test(
+        'When counter class instantiated then the value of count should be 0', // this is just a msg
+        () {
+      final Counter counter = Counter();
+
+      final val = counter.count;
+
+      expect(val, 0);
+    });
+
+    test('When counter is incremented then the value of count should be 1', () {
+      final Counter counter = Counter();
+
+      counter.incrementCounter();
+      final val = counter.count;
+
+      expect(val, 1);
+    });
+
+    test('When counter is decremented then the value of count should be -1',
+        () {
+      final Counter counter = Counter();
+
+      counter.decrementCounter();
+      final val = counter.count;
+
+      expect(val, -1);
+    });
+  });
 }
